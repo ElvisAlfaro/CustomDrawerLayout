8f4f6df9672a1e4fda9740e986e6090f3e42f8f3
ElvisAlfaro <ej.aahbk@gmail.com>
2023-08-16 00:37:40 -0500

    Toolbar with menu XML

--- app/src/main/java/com/example/customdrawerlayout/MainActivity.kt
+++ app/src/main/java/com/example/customdrawerlayout/MainActivity.kt
@@ -1,8 +1,11 @@
 package com.example.customdrawerlayout
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.customdrawerlayout.databinding.ActivityMainBinding
+import com.example.customdrawerlayout.with_menu_xml.WithMenuXmlActivity
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     private val binding: ActivityMainBinding by lazy {
@@ -12,5 +15,20 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        initListener()
+    }
+
+    private fun initListener() {
+        binding.apply {
+            btnDrawer1.setOnClickListener {
+                startActivity(
+                    Intent(this@MainActivity, WithMenuXmlActivity::class.java)
+                )
+            }
+            btnDrawer2.setOnClickListener {
+                Snackbar.make(binding.root, "Proximamente", Snackbar.LENGTH_SHORT).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/example/customdrawerlayout/with_menu_xml/WithMenuXmlActivity.kt
+++ app/src/main/java/com/example/customdrawerlayout/with_menu_xml/WithMenuXmlActivity.kt
@@ -2,6 +2,10 @@ package com.example.customdrawerlayout.with_menu_xml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import com.example.customdrawerlayout.R
 import com.example.customdrawerlayout.databinding.ActivityWithMenuXmlBinding
 
@@ -13,5 +17,67 @@ class WithMenuXmlActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+
+        setSupportActionBar(binding.mtToolbar)
+
+        val toggle =ActionBarDrawerToggle(this,
+            binding.dlDrawer,
+            binding.mtToolbar,
+            R.string.close_drawer,
+            R.string.open_drawer
+        )
+        binding.dlDrawer.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.nvMenuLateral.setNavigationItemSelectedListener {
+            onNavigationItemSelected(it)
+            true
+        }
     }
+
+    override fun onBackPressed() {
+        binding.dlDrawer.run {
+            if (isDrawerOpen(GravityCompat.START))
+                closeDrawer(GravityCompat.START)
+            else
+                super.onBackPressed()
+        }
+    }
+
+    private fun onNavigationItemSelected(item: MenuItem) {
+        simpleAction(item)
+    }
+
+    private fun simpleAction(item: MenuItem) {
+        var message = ""
+        when(item.itemId){
+            R.id.menu_call-> {
+                binding.mtToolbar.setTitle(R.string.menu_call)
+                message = "Menu Call"
+            }
+            R.id.menu_camera-> {
+                binding.mtToolbar.setTitle(R.string.menu_camera)
+                message = "Menu camera"
+            }
+            R.id.menu_gallery-> {
+                binding.mtToolbar.setTitle(R.string.menu_gallery)
+                message = "Menu Gallery"
+            }
+            R.id.menu_send-> {
+                binding.mtToolbar.setTitle(R.string.menu_send)
+                message = "Menu Send"
+            }
+            R.id.menu_share-> {
+                binding.mtToolbar.setTitle(R.string.menu_share)
+                message = "Menu Share"
+            }
+            R.id.menu_agenda-> {
+                binding.mtToolbar.setTitle(R.string.menu_agenda)
+                message = "Menu Agenda"
+            }
+        }
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+        binding.dlDrawer.closeDrawer(GravityCompat.START)
+    }
+
 }
